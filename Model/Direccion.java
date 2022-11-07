3835da30dcccd3be66648840e5aa85ccf4083524
littlesonik <sonik.zapata@gmail.com>
2022-11-07 16:13:39 -0300

    UML modelado de base de datos

---
+++ Model/Direccion.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Direccion {
+
+    /**
+     * Default constructor
+     */
+    public Direccion() {
+    }
+
+    /**
+     * 
+     */
+    private string calle;
+
+    /**
+     * 
+     */
+    private void attribute1;
+
+    /**
+     * 
+     */
+    private string comuna;
+
+    /**
+     * 
+     */
+    private string ciudad;
+
+    /**
+     * 
+     */
+    private string region;
+
+}
---
+++ Model/Telefono.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Telefono {
+
+    /**
+     * Default constructor
+     */
+    public Telefono() {
+    }
+
+    /**
+     * 
+     */
+    private int numero;
+
+    /**
+     * 
+     */
+    private string codigo;
+
+    /**
+     * 
+     */
+    private string compañia;
+
+    /**
+     * 
+     */
+    private boolean pospago;
+
+}
